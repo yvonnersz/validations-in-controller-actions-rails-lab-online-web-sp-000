57d04215ae2796c76dc5dd7835c81f36907db88d
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-17 07:26:49 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,8 +8,12 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create!(author_params)
-    redirect_to author_path(@author)
-
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
