fc3150c85ec9de01846f5ab779ddb2e7f11cf436
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-17 07:15:41 +0000

    Automatically backed up by Learn

--- app/validators/name_validator.rb
+++ app/validators/name_validator.rb
@@ -2,8 +2,6 @@ class NameValidator < ActiveModel::Validator
   def validate(record)
     if record.name == nil
       record.errors[:name] << "Please enter a name."
-    else
-      record.errors[:email] << "Email has already been taken."
     end
   end
 end
