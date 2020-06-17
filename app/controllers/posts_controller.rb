21a572353c485fda255c3316b3c9fedc9ff1bc5c
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-17 07:24:52 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,7 @@ class PostsController < ApplicationController
 
   def update
     @post.update(post_params)
-
-    if @post.valid?
-      @post.save
       redirect_to post_path(@post)
-    else
-      render :edit
-    end
-
   end
 
   private
